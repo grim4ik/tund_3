de986f5aff7bfee869bde0c477feeb3f4af0f595
kirikotk <kirikotk@greeny.cs.tlu.ee>
2016-09-14 14:49:13 +0300

    tund_3

---
+++ second.php
@@ -0,0 +1,36 @@
+<?php
+    
+	$age = 5;
+	
+	if ($age < 18)  {
+		
+		echo "alaealine";
+		
+		} else {
+			
+			echo "täisealine";
+			
+			}
+		
+
+
+
+?>
+
+<br>
+
+<?php
+    
+	//kord kõige algus $i = 0
+	// SISU iga kord $i <$
+	//
+	for ($i = 1; $i > $age; $i = $i + 1 )		{
+		
+		echo $i. "palju ";
+		
+	}
+	
+	echo "õnne";
+	
+
+?>

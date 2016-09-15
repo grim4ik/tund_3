3d39cba4b1fc8096c85455ac7a9c24d97e4d1f63
kirikotk <kirikotk@greeny.cs.tlu.ee>
2016-09-15 15:05:47 +0300

    login.php

--- login.php
+++ login.php
@@ -1,3 +1,43 @@
+<?php
+
+	//var_dump($_GET);
+	
+	//echo "<br>";
+	
+	//var_dump($_POST);
+	
+		$singupEmailError = "*";
+
+	//kas keegi vajutas nuppu ja see on olemas
+	
+	if (isset($_POST["singupEmail"])) {
+		
+		//on oleam
+		// kas epost on tühi
+		if (empty($_POST["sinupEmail"])) {
+			// on tühi
+			
+			$singupEmailError = "*Väli on kosustuslik";
+		}
+	}
+	
+	//////////////////////////////////////////////////////
+	
+			$singupPasswordError = "*";
+
+	
+	if (isset($_POST["singupPassword"])) {
+		
+
+		if (empty($_POST["sinupPassword"])) {
+
+		
+			$singupPasswordError = "*Väli on kosustuslik";
+		}
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -12,6 +52,7 @@
 		<input name="loginEmail" type="email">
 		
 		<br><br>
+		
 		<label>Parool</label><br>
 		<input name="loginPassword" type="password">
 		
@@ -25,11 +66,11 @@
 	<h1>Loo kasutaja</h1>
 	
 	<form method="POST" >
-		<input name="loginEmail" placeholder="E-post" type="email">
+		<input name="singupEmail" placeholder="E-post" type="email">  <?php echo $singupEmailError; ?>
 		
 		<br><br>
 		
-		<input name="loginPassword" placeholder="Parool" type="password">
+		<input name="singupPassword" placeholder="Parool" type="password">  <?php echo $singupPasswordError; ?>
 		
 		<br><br>
 		
@@ -37,8 +78,6 @@
 		
 	</form>
 		
-	
-
 	</body>
 </html>
 

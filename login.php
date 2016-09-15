5c12aeca73893ed6cfd317142041573705a5634f
kirikotk <kirikotk@greeny.cs.tlu.ee>
2016-09-15 14:41:45 +0300

    login.php

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+	<h1>Logi sesse</h1>
+	
+	<form method="POST" >
+	    <label>E-post</label><br>
+		<input name="loginEmail" type="email">
+		
+		<br><br>
+		<label>Parool</label><br>
+		<input name="loginPassword" type="password">
+		
+		<br><br>
+		
+		<input type="submit" value="Logi sisse">
+		
+	</form>
+	
+	
+	<h1>Loo kasutaja</h1>
+	
+	<form method="POST" >
+		<input name="loginEmail" placeholder="E-post" type="email">
+		
+		<br><br>
+		
+		<input name="loginPassword" placeholder="Parool" type="password">
+		
+		<br><br>
+		
+		<input type="submit" value="Loo kasutaja">
+		
+	</form>
+		
+	
+
+	</body>
+</html>
+
